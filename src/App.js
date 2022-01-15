e8bb3927e831a4c0e7cad3accd63ab062dd15f32
Prafful Lachhwani <prafful.lachhwani@gmail.com>
2022-01-15 18:55:54 +0530

    Added medium style text editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DraftailEditor } from "draftail";
+import { EditorState } from "draft-js";
+import createInlineToolbarPlugin from "draft-js-inline-toolbar-plugin";
+import createSideToolbarPlugin from "draft-js-side-toolbar-plugin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "draft-js/dist/Draft.css";
+import "draftail/dist/draftail.css";
+import "draft-js-inline-toolbar-plugin/lib/plugin.css";
+import "draft-js-side-toolbar-plugin/lib/plugin.css";
+
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+
+const sideToolbarPlugin = createSideToolbarPlugin();
+const { SideToolbar } = sideToolbarPlugin;
+
+const plugins = [inlineToolbarPlugin, sideToolbarPlugin];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState(state) {
+    this.setState({
+      editorState: state
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DraftailEditor
+          editorState={this.state.editorState}
+          onChange={this.changeState}
+          placeholder="Tell your story..."
+          plugins={plugins}
+        />
+        <InlineToolbar />
+        <SideToolbar />
+      </div>
+    );
+  }
 }
 
 export default App;
